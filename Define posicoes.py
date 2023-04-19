6f41be750e1886431101cce16c860d0dce174d73
Guido <guidomandelert@gmail.com>
2023-04-19 09:16:03 -0300

    Vai dessa vez

--- Define posicoes.py
+++ Define posicoes.py
@@ -1,11 +1,9 @@
 def define_posicoes(linha, coluna, orientacao ,tamanho):
-    tam=int(tamanho)
     base=[0]*tamanho
-    for i in range(tam):   
+    for i in range(int(tamanho)):   
+        base[i]=[linha,coluna]
         if orientacao=='vertical':
-            base[i]=[linha,coluna]
             linha+=1
         if orientacao=='horizontal':
-            base[i]=[linha,coluna]
             coluna+=1
     return base
